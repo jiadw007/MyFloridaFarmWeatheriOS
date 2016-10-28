97a0a7711bc2b70c5eabcdcf08ef59877fa8244b
jiadw007 <jiadw007@gmail.com>
2016-10-27 23:25:54 -0400

    Initial Commit

---
+++ mffw-mobile-app-v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mffw-mobile-app-v2
+//
+//  Created by FAWN on 10/27/16.
+//  Copyright © 2016 IFAS/IT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
